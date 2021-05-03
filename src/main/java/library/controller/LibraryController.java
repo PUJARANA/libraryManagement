9f2fbc930bddd0829b4e5852120ec62b33c753db
PUJARANA <65430980+PUJARANA@users.noreply.github.com>
2021-05-03 16:00:24 -0400

    Test Commit

--- src/main/java/library/controller/LibraryController.java
+++ src/main/java/library/controller/LibraryController.java
@@ -1,7 +1,28 @@
 package library.controller;
 
+import library.model.LibraryRespDto;
+import library.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+This controller class is the entry point to all incoming request
+@author:Pooja
+ */
+@RestController
+@RequestMapping("library/books")
 public class LibraryController {
 
+    @Autowired
+    private LibraryService libraryService;
+
+    @GetMapping
+    public LibraryRespDto getBooks(){
+
+    return libraryService.getBooks();
+    }
 }
 
 
