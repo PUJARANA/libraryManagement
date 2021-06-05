4d4f144c5baa94207e0025335e6404b0827c16d4
PUJARANA <65430980+PUJARANA@users.noreply.github.com>
2021-06-05 17:47:07 -0400

    "commit added files"

---
+++ src/main/java/library/learning/TestGit.java
@@ -0,0 +1,5 @@
+package library.learning;
+
+public class TestGit {
+    int i=1;
+}
---
+++ target/classes/import.sql
@@ -0,0 +1,16 @@
+INSERT into books(id,name,author_name,copies) values (100, 'rich dad poor dad', 'Ramesh', 4);
+INSERT into books(id,name,author_name,copies) values (101, 'war and peace', 'Khilan', 5);
+INSERT into books(id,name,author_name,copies) values (103, 'head first java', 'kaushik', 7);
+INSERT into books(id,name,author_name,copies) values (104, 'think python', 'Chaitali', 0);
+INSERT into books(id,name,author_name,copies) values (105, 'when spring comes', 'Hardik', 8);
+INSERT into books(id,name,author_name,copies) values (106, 'spring boot in action', 'Komal', 0);
+INSERT into books(id,name,author_name,copies) values (107, 'head first kotlin', 'Muffy', 5);
+
+
+INSERT into user(id,name,email,books_limit) values (11, 'Ramesh', 'Ahmedabad@gmail.com', 2);
+INSERT into user(id,name,email,books_limit) values (12, 'Khilan', 'Delhi@gmail.com', 2);
+INSERT into user(id,name,email,books_limit) values (13, 'kaushik', 'Kota@gmail.com', 2);
+INSERT into user(id,name,email,books_limit) values (14, 'Chaitali', 'Mumbai@gmail.com', 2);
+INSERT into user(id,name,email,books_limit) values (15, 'Hardik', 'Bhopal@gmail.com', 2);
+INSERT into user(id,name,email,books_limit) values (16, 'Komal', 'MP@gmail.com', 2);
+INSERT into user(id,name,email,books_limit) values (17, 'Muffy', 'Indore@gmail.com', 2);
